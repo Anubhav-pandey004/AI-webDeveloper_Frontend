49d657178b5dc7a386967fd5e86077807a254c66
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-05-28 10:55:17 +0530

    Update socket.jsx

--- src/common/socket.jsx
+++ src/common/socket.jsx
@@ -1,4 +1,4 @@
-import { io } from 'socket.io-client';
+import socket from 'socket.io-client';
 
 
 let socketInstance = null;
@@ -8,28 +8,23 @@ let socketInstance = null;
 export const initializeSocket = (projectId) => {
     if (!socketInstance) {
         
-        socketInstance = io('https://ai-webdeveloper-backend.onrender.com', {
-            timeout: 20000,
+        socketInstance = socket('https://ai-webdeveloper-backend.onrender.com', {
             auth: {
                 token: localStorage.getItem('token')  // or document.cookie for cookie-based tokens
             },
             query: {
                 projectId
             },
-             transports: ['websocket'],
+            transports: ['websocket'],
         });        
 
         socketInstance.on('connection', () => {
-             console.log("Connected to socket server", socketInstance.id);
+
         });
 
         socketInstance.on('project-message-receive', (data) => {
-            console.log("Disconnected from socket server");
-        });
-        socketInstance.on('connect_error', (err) => {
-           console.error("Socket connection error:", err.message);
-        });
 
+        });
     }
     return socketInstance;
 };
@@ -43,6 +38,51 @@ export const receiveMessage = (eventNames, cb)=>{
 export const sendMessage = (eventNames, data)=>{    
     socketInstance.emit(eventNames, data);
 }
+// import { io } from 'socket.io-client';
+
+
+// let socketInstance = null;
+
+// //socket instance variable is now our path to send msg to backend
+// //we can emmit any event
+// export const initializeSocket = (projectId) => {
+//     if (!socketInstance) {
+        
+//         socketInstance = io('https://ai-webdeveloper-backend.onrender.com', {
+//             timeout: 20000,
+//             auth: {
+//                 token: localStorage.getItem('token')  // or document.cookie for cookie-based tokens
+//             },
+//             query: {
+//                 projectId
+//             },
+//              transports: ['websocket'],
+//         });        
+
+//         socketInstance.on('connection', () => {
+//              console.log("Connected to socket server", socketInstance.id);
+//         });
+
+//         socketInstance.on('project-message-receive', (data) => {
+//             console.log("Disconnected from socket server");
+//         });
+//         socketInstance.on('connect_error', (err) => {
+//            console.error("Socket connection error:", err.message);
+//         });
+
+//     }
+//     return socketInstance;
+// };
+
+
+// //various events
+// export const receiveMessage = (eventNames, cb)=>{
+//     socketInstance.on(eventNames, cb);
+// }
+
+// export const sendMessage = (eventNames, data)=>{    
+//     socketInstance.emit(eventNames, data);
+// }
 
 
 
